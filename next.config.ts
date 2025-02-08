64fdf187abc234394ec4fba42f3992c88b2060de
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2025-02-09 00:43:09 +0200

    first cars

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains:[`cdn.imagin.studio`]
-  }
+  images: {
+    domains: [`cdn.imagin.studio`],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
